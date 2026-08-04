a382cbd215533e13fd4bcdba66b31295b169512a
gokul-github-collab <gokulakrishnan0604@gmail.com>
2026-08-04 14:41:36 +1200

    Add main function to fibonacci activity and ask for a user input

--- week1_activity.py
+++ week1_activity.py
@@ -1,10 +1,12 @@
-print("Hello Python")
 
-def fibonnaci(n):
+def fibonacci(n):
     if n == 0 or n == 1:
         return n
 
-    print(fibonnaci(n - 1) + fibonnaci(n - 2))
-    return fibonnaci(n - 1) + fibonnaci(n - 2)
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
-print(fibonnaci(7))
+
+
+if __name__ == "__main__":
+    user_input = int(input("Enter a number: "))
+    print(fibonacci(user_input))

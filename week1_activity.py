430de9e3a2a224ea14c6e4aa40a6d59c123a6272
gokul-github-collab <gokulakrishnan0604@gmail.com>
2026-08-04 14:49:40 +1200

    write a function to calculate factorial of n

--- week1_activity.py
+++ week1_activity.py
@@ -6,7 +6,17 @@ def fibonacci(n):
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
+def factorial(n):
+    result = 1
+    for i in range(1,n + 1):
+        result *= i
+
+    return result
+
+
 
 if __name__ == "__main__":
     user_input = int(input("Enter a number: "))
     print(fibonacci(user_input))
+
+    print(f'Factorial of {user_input}: {factorial(user_input)}')
